f69ca9c464ce1a9aa484bb95427b978606f91472
Jeff Kolesky <jeff@kolesky.com>
2012-09-19 11:00:44 -0700

    Using awk instead of cut

--- ruby/prompt.bash
+++ ruby/prompt.bash
@@ -1,7 +1,7 @@
 # Sets up prompt functions to use with rbenv
 function __rbenv_ps1() {
     if which rbenv > /dev/null; then
-        local ruby_version=$(rbenv version | cut -d' ' -f 1)
+        local ruby_version=$(rbenv version | awk '{ print $1 }')
         if [ "$ruby_version" != "system" ]; then
             echo -e "|$ruby_version|"
         fi
