0cfce81d55ab7e566cc9e10740a8c6bfbc6ff253
Jeff Kolesky <jeff@kolesky.com>
2012-10-25 17:09:50 -0700

    Upgrades to CDH4.1.1

--- hadoop/cdh.bash
+++ hadoop/cdh.bash
@@ -1,9 +1,12 @@
 hadoop_root=/opt/hadoop
+
 cdh3_hadoop_home=$hadoop_root/hadoop-0.20.2-cdh3u4
-cdh4_hadoop_home=$hadoop_root/hadoop-2.0.0-mr1-cdh4.0.1
-cdh3_wibi_home=/opt/wibi-1.7.1
+cdh4_hadoop_home=$hadoop_root/hadoop-2.0.0-cdh4.1.1
+
 cdh3_hbase_home=$hadoop_root/hbase-0.90.6-cdh3u4
-cdh4_hbase_home=$hadoop_root/hbase-0.92.1-cdh4.0.1
+cdh4_hbase_home=$hadoop_root/hbase-0.92.1-cdh4.1.1
+
+cdh3_wibi_home=/opt/wibi-1.7.1
 cdh4_wibi_home=/opt/wibi-2.0.0
 
 function __add_hadoop_to_path() {
