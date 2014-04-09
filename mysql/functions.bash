2fb86fb320f570bf260bbc78cc9a371ca41b9a54
Jeff Kolesky <jeff@kolesky.com>
2014-04-09 13:22:48 -0700

    Adds tailmysql function

---
+++ mysql/functions.bash
@@ -0,0 +1,9 @@
+function tailmysql() {
+  logfile=$(mysql -BANe "show variables like 'general_log_file'" | cut -f2)
+  logging=$(mysql -BANe "show variables like 'general_log'" | cut -f2)
+  if [[ "$logging" == "OFF" ]]; then
+    mysql -BANe "set global general_log = 'ON'"
+  fi
+  tail -f $logfile
+  mysql -BANe "set global general_log = 'OFF'"
+}
