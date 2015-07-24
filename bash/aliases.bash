313c68f89dad25338a2f2024ec2737704bf4fd37
Jeff Kolesky <jeff@kolesky.com>
2015-07-24 08:58:15 -0700

    Adds default ts format

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -3,3 +3,4 @@ alias fj='find . -name "*.java"'
 alias fs='find . -name "*.scala"'
 alias pivot="perl -pe 's/\t/\n/g'"
 alias pivotl="perl -pe 's/\t/\n/g' | nl -ba"
+alias ts="ts '[%Y-%m-%d %H:%M:%.S]'"
