cbde159c30d4b491e75961fda13cbd98884be693
Jeff Kolesky <jeff@kolesky.com>
2015-10-12 12:11:44 -0700

    Attempts to fix tailpostgres
    The problem is that the commands to reset the log level after canceling the tail
    do not always work. It worked great for mysql, but not so great for postgres.
    I'm not too sure why, but this is an attempt to fix it.

--- postgres/functions.bash
+++ postgres/functions.bash
@@ -1,8 +1,20 @@
-function tailpostgres() {
-  conf=/usr/local/var/postgres/postgresql.conf
-  sed -i .bak "s/#log_statement = 'all'/log_statement = 'all'/" $conf
-  pg_ctl reload
-  tail -f /usr/local/var/postgres/server.log
+
+conf=/usr/local/var/postgres/postgresql.conf
+
+function canceltail() {
+  echo cancelling tail $_TAIL_PID
+  kill $_TAIL_PID
+  unset _TAIL_PID
   sed -i .bak "s/log_statement = 'all'/#log_statement = 'all'/" $conf
   pg_ctl reload
+  echo reloaded pg
+}
+
+function tailpostgres() {
+  sed -i .bak "s/#log_statement = 'all'/log_statement = 'all'/" $conf
+  pg_ctl reload
+  trap canceltail INT
+  tail -f /usr/local/var/postgres/server.log &
+  export _TAIL_PID=$!
+  wait $_TAIL_PID
 }
