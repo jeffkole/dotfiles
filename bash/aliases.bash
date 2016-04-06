6807bb77eb9178b1838cae4af06025addc945aa6
Jeff Kolesky <jeff@kolesky.com>
2016-04-06 15:31:22 -0700

    Adds weather alias

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,3 +4,4 @@ alias fs='find . -name "*.scala"'
 alias pivot="perl -pe 's/\t/\n/g'"
 alias pivotl="perl -pe 's/\t/\n/g' | nl -ba"
 alias ts="ts '[%Y-%m-%d %H:%M:%.S]'"
+alias weather="curl wttr.in/San_Francisco"
